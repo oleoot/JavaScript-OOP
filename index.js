a8d51ea593103464630adcafbf470e7cbe6a303e
oleoot <oleoot28@gmail.com>
2020-07-28 11:52:08 +0300

    Factory functions

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function () {
-        console.log("Draw")
-    }
-}
 // Function - method
 // Variables - properties
+// If object has one or more methods - it has behaviour
 
+
+// Object literals =============================================
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     draw: function () {
+//         console.log("Draw")
+//     }
+// }
+// circle.draw()
+
+// Factory function ===================================================
+
+function createCircle(radius) {
+    return {
+        radius: radius,
+        draw: function () {
+            console.log('draw')
+        }
+    }
+}
+const circle = createCircle(1)
 circle.draw()
