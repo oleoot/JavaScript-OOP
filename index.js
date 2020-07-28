5f2a568b61560e0c7ccc96783e70c19d6d98fadc
oleoot <oleoot28@gmail.com>
2020-07-28 12:17:34 +0300

    Primitives and reference types

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Function - method
 // Variables - properties
 // If object has one or more methods - it has behaviour
+// Value types - primitives (Number, String, Boolean, Symbol, undefinde, null)
+// Reference types - objects (object, function, array)
+// Primitives are copied by their value
+// Reference types are copied by their reference(относяться к одному месту в памяти)
+
+
+
+
+
 
 
 // Object literals =============================================
@@ -32,11 +41,63 @@
 
 // Constructor function ===================================================
 
-function Circle(radius) {
-    this.radius = radius
-    this.draw = function () {
-        console.log('draw')
-    }
-}
-const circle = new Circle(1)
-circle.draw()
+// function Circle(radius) {
+//     this.radius = radius
+//     this.draw = function () {
+//         console.log('draw')
+//     }
+// }
+// const circle = new Circle(1)
+// circle.draw()
+
+
+
+
+//Primitives ====================================================================================
+
+// let x = 10;
+// let y = x;
+
+// x = 20
+
+// console.log(x, y)
+
+// Reference type =================================================================================
+
+// let x = {
+//     value: 10
+// }
+// let y = x
+
+// x.value = 20
+
+// console.log(x, y)
+
+
+
+//Primitives ====================================================================================
+
+// let number = 10
+
+// function increase(number) {
+//     number++;
+// }
+
+// increase(number)
+
+// console.log(number)
+
+
+// Reference type =================================================================================
+
+// let obj = {
+//     value: 10
+// }
+
+// function increase(obj) {
+//     obj.value++;
+// }
+
+// increase(obj)
+
+// console.log(obj)
